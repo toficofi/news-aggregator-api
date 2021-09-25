c18381d45d1aaf80883a035ca1998fec30f28643
Josh Lee <josh@joshbe.me>
2021-09-25 21:27:15 +0100

    first commit

---
+++ Settings/MongoSettings.cs
@@ -0,0 +1,8 @@
+namespace NewsAggregator.Settings 
+{
+    public sealed class MongoSettings 
+    {
+        public string ConnectionString {get;set;}
+        public string DatabaseName {get;set;}
+    }
+}
