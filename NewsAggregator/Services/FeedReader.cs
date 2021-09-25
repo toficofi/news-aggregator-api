ea596c07b29c22e04957d7efad7ec76635287090
Josh Lee <josh@joshbe.me>
2021-09-25 21:36:45 +0100

    Move into solution

---
+++ NewsAggregator/Services/FeedReader.cs
@@ -0,0 +1,12 @@
+using NewsAggregator.Settings;
+
+namespace NewsAggregator.Services
+{
+    public sealed class FeedReader
+    {
+        public FeedReader(MongoSettings mongoSettings) 
+        {
+            
+        }
+    }
+}
